c7fe198adb48a72487d3a69fe486ccd34c7bdb6b
nbah1990 <ivan.karatayev@gmail.com>
2020-11-10 15:22:53 +0600

    - changed config loading logic;

--- src/LaravelLoggerExtension/LaravelLoggerExtensionServiceProvider.php
+++ src/LaravelLoggerExtension/LaravelLoggerExtensionServiceProvider.php
@@ -6,22 +6,36 @@ namespace LaravelLoggerExtension;
 use GuzzleHttp\Client;
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Foundation\Application as LaravelApplication;
+use Laravel\Lumen\Application as LumenApplication;
+
 class LaravelLoggerExtensionServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $source = realpath($raw = __DIR__ . '/./config/laravel-logger-extension.php') ?: $raw;
-
-        $this->publishes([$source => config_path('laravel-logger-extension.php')], 'config');
-        $this->mergeConfigFrom($source, 'laravel-logger-extension');
+        $this->setupConfig();
+        $this->setupGuzzleIntegration();
     }
 
-    public function register()
-    {
+
+    protected function setupGuzzleIntegration() {
         if (config('laravel-logger-extension.guzzle_log_integration_enabled') === true) {
             $this->app->bind(Client::class, function () {
                 return new \LaravelLoggerExtension\Guzzle\Client(config('laravel-logger-extension.guzzle_log_integration'));
             });
         }
+    }
+
+    protected function setupConfig()
+    {
+        $source = realpath(__DIR__.'/config/laravel-logger-extension.php');
+
+        if ($this->app instanceof LaravelApplication) {
+            $this->publishes([$source => config_path('laravel-logger-extension.php')]);
+        } elseif ($this->app instanceof LumenApplication) {
+            $this->app->configure('laravel-logger-extension');
+        }
+
+        $this->mergeConfigFrom($source, 'laravel-logger-extension');
     }
 }
